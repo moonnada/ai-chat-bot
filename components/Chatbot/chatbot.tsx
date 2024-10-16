2546e2a482016cd7d4272a4f5d8233ca4b26ec10
moonnada <kimbm2014@gmail.com>
2024-10-16 14:35:07 -0700

    init commit

---
+++ components/Chatbot/chatbot.tsx
@@ -0,0 +1,9 @@
+import { TbMessageChatbot } from "react-icons/tb"
+
+export default function Chatbot() {
+    return (
+        <>
+            <TbMessageChatbot size={64} className="fixed right-24 bottom-[calc(1rem)] hover:cursor" />
+        </>
+    )
+}
