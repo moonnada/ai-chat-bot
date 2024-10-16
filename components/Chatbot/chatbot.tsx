01d04c3d6ea15a8cf904e8159f5825b567e2722c
moonnada <kimbm2014@gmail.com>
2024-10-16 15:08:11 -0700

    Worked chatbot design

--- components/Chatbot/chatbot.tsx
+++ components/Chatbot/chatbot.tsx
@@ -1,9 +1,43 @@
+'use client';
+import { useState } from "react"
 import { TbMessageChatbot } from "react-icons/tb"
+import BotMessage from "./ui/bot-message";
+import UserMessage from "./ui/user-message";
+import ChatInput from "./ui/chat-input";
+
+
 
 export default function Chatbot() {
+    const [showChat, setShowChat] = useState(false)
+
+
     return (
         <>
-            <TbMessageChatbot size={64} className="fixed right-24 bottom-[calc(1rem)] hover:cursor" />
+            <TbMessageChatbot size={64}
+                onClick={() => setShowChat(!showChat)}
+                className="fixed right-24 bottom-[calc(1rem)] hover:cursor-pointer" />
+
+            {showChat && (
+                <div className="fixed right-12 bottom-[calc(4rem+1.5rem)] border hover:cursor-pointer p-5 shadow-md shadow-white h-[474px] w-[500px]">
+                    <div className="flex flex-col h-full">
+                        {/* {CHAT HEADER} */}
+                        <div>
+                            <h2 className="font-semibold text-lg trcking-tight">Chatbot</h2>
+                            <p>Power by Open AI </p>
+                        </div>
+
+                        {/* {CHAT CONTAINER} */}
+                        <div className="flex flex-col flex-l items-center p-2 mt-5 overflow-y-auto">
+                            <BotMessage />
+                            <UserMessage />
+                        </div>
+
+                        {/* {MESSAGE INPUT} */}
+                        <ChatInput />
+
+                    </div>
+                </div>
+            )}
         </>
     )
 }
